f2b6b14ba25f56300d1032793aaa0c084911e7cb
Trophyemo <107163180+Trophyemo@users.noreply.github.com>
2022-06-16 11:14:14 +0800

    Update WAU.scala

--- spark-2.x/src/main/scala/com/aliyun/odps/spark/examples/WAU.scala
+++ spark-2.x/src/main/scala/com/aliyun/odps/spark/examples/WAU.scala
@@ -8,11 +8,6 @@ object WAU {
       .builder()
       .appName("WAU")
       .config("spark.master", "local[4]") // 需设置spark.master为local[N]才能直接运行，N为并发数
-      .config("spark.hadoop.odps.project.name", "graviti_bi")
-      .config("spark.hadoop.odps.access.id", "LTAI5tE3bzCSuJ2QY5dfGNa8")
-      .config("spark.hadoop.odps.access.key", "QCqKValsq6IzU2G7UvO8Cxr1vPoXsY")
-      .config("spark.hadoop.odps.end.point", "http://service.cn.maxcompute.aliyun.com/api")
-      .config("spark.sql.catalogImplementation", "odps")
       .getOrCreate()
 
     try {
